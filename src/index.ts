d7e3c99afaef3c22c3f3f840e19e025f80fd6873
mustafa <mustafa@dwekat.com>
2022-01-31 19:25:36 +0400

    first code implementation

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Edge } from 'edge.js';
+import { EngineOptions } from './interfaces/engine-options.interface';
+
+/**
+ *
+ * @param {EngineOptions} params
+ * @return {(filePath: string, data: any, next) => void}
+ */
+export function createEngine(params: EngineOptions) {
+  const edge = new Edge(params.options);
+  edge.mount(params.viewsRoot);
+
+  return function edgeTemplateEngine(filePath: string, data: any, next) {
+    //eslint-disable-next-line @typescript-eslint/no-var-requires
+    const html = edge.renderSync(filePath, data);
+    next(null, html);
+  };
+}
